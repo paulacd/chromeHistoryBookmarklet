6b7f6346a9aa1b1d1bcca8db12b88c4c4dffadb2
paulacd <pcd250@nyu.edu>
2017-03-08 16:04:04 -0500

    added alert for new tab opened

--- background.js
+++ background.js
@@ -1,32 +1,34 @@
 console.log('hi');
 var urls = [];
-var cleanUrls = [];
 
-chrome.browserAction.onClicked.addListener(function () {
-  console.log('registered click');
-  getHistory();
+chrome.browserAction.onClicked.addListener(function() {
+    console.log('registered click');
+    getHistory();
 })
 
-function getHistory(){
-  console.log('inside getHistory');
-  var millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-  var oneWeekAgo = (new Date).getTime() - millisecondsPerWeek;
-  var twoWeeksAgo = (new Date).getTime() - millisecondsPerWeek * 2;
+function getHistory() {
+    console.log('inside getHistory');
+    var millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+    var oneWeekAgo = (new Date).getTime() - millisecondsPerWeek;
+    var twoWeeksAgo = (new Date).getTime() - millisecondsPerWeek * 2;
 
-  chrome.history.search({
-    'text': '',              // Return every history item....
-    'startTime': twoWeeksAgo,  // that was accessed less than one week ago.
-    'endTime': oneWeekAgo,
-    'maxResults': 1
-  },
-  function (historyItems){
-    //debugger;
-    var firstItem = historyItems[0];
-    if (firstItem) {
-      var url = firstItem.url;
-      //alert(url);
-      chrome.tabs.create({
-        'url': url,
-      });
-    }
-    
+    chrome.history.search({
+                'text': '', // Return every history item....
+                'startTime': twoWeeksAgo, // that was accessed less than one week ago.
+                'endTime': oneWeekAgo,
+                'maxResults': 1
+            },
+            function(historyItems) {
+                //debugger;
+                var firstItem = historyItems[0];
+                if (firstItem) {
+                    var url = firstItem.url;
+                    //alert(url);
+                    chrome.tabs.create({
+                        'url': url,
+                    });
+                }
+                alert ("You were looking at this page on" + new Date(oneWeekAgo));
+            });
+
+          }
